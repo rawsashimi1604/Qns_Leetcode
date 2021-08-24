bed0f4e61ca1cf94d577dfe408d26461b7742981
Gavin Loo <looweiren@gmail.com>
2021-08-24 19:21:14 +0800

    added reduce-to-zero.py

---
+++ leetcode_py/reduce-to-zero.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        counter = 0 
+        while num:
+            if num % 2 == 0:
+                num = num / 2
+            else:
+                num = num - 1
+            
+            counter += 1
+        
+        return counter
