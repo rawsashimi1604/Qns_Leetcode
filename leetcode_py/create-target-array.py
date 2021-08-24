cc72669886c4e3909f07931b08e9dd5917a6676d
Gavin Loo <looweiren@gmail.com>
2021-08-24 20:10:39 +0800

    Added create-target-array.py

---
+++ leetcode_py/create-target-array.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        target = []
+        for i in range(len(nums)):
+            target.insert(index[i], nums[i])
+            
+        return target
+            
+            
