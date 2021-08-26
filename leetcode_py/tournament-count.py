4f8d1c3bc45b725dd6d0a78c9af6850c45ee9f96
Gavin Loo <looweiren@gmail.com>
2021-08-26 15:13:49 +0800

    added tournament-count.py

---
+++ leetcode_py/tournament-count.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        matches = 0
+
+        while n != 1:
+            if n % 2 == 0:
+                matches += n//2
+                n = n // 2
+
+            else:
+                matches += 1
+                matches += (n-1) // 2
+                n = (n - 1) // 2
+
+        return matches
+
+
+test = Solution()
+print(test.numberOfMatches(7))
