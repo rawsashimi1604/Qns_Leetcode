affa542763eaad2707f12b7ff660c15cc5dda7ec
Gavin Loo <looweiren@gmail.com>
2021-09-06 17:21:01 +0800

    added maximum-69.py

---
+++ leetcode_py/maximum-69.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maximum69Number(self, num: int) -> int:
+
+        res = [num]
+        arr = [int(x) for x in str(num)]
+
+        for i in range(len(arr)):
+            arr = [int(x) for x in str(num)]
+            tmp = ""
+            if arr[i] == 6:
+                arr[i] = 9
+            else:
+                arr[i] = 6
+
+            for j in range(len(arr)):
+                tmp += str(arr[j])
+
+            res.append(int(tmp))
+
+        print(res)
+        return max(res)
+
+
+test = Solution()
+test.maximum69Number(9669)
